b20417735e7b407edd4bf7ad1b2b961120a751c8
nitin kumawat <nitinkumawat985@gmail.com>
2026-08-01 00:05:18 +0530

    refactor: expose printing feature boundary

---
+++ apps/web/src/features/hardware/printing/index.ts
@@ -0,0 +1,12 @@
+export { openIsolatedBillPrint } from "./browser/open-isolated-bill-print";
+export { buildIsolatedPrintDocument } from "./core/build-isolated-print-document";
+export {
+  A4_PRINT_PAGE_RULE,
+  BILL_PRINT_ROOT_SELECTOR,
+  DEFAULT_PRINT_WINDOW_FEATURES,
+  NON_PRINT_SELECTOR,
+} from "./core/print-contract";
+export type {
+  IsolatedPrintDocumentInput,
+  PrintPreparationResult,
+} from "./core/print-contract";
