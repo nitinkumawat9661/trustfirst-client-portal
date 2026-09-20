67d1f13d95d51a386bdfa90ad0331a42a6a8f556
nitin kumawat <nitinkumawat985@gmail.com>
2026-09-20 14:54:00 +0530

    feat(celebration): type catalog image uploads

--- celebration-site/config/storage.ts
+++ celebration-site/config/storage.ts
@@ -2,3 +2,4 @@ import rawStorageConfig from "./storage.json"
 
 export const storageConfig = rawStorageConfig
 export type PackingVideoMimeType = keyof typeof storageConfig.packingVideoTypes
+export type CatalogImageMimeType = keyof typeof storageConfig.catalogImageTypes
